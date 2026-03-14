39a1640c3c65b9e62ebe2aa0bacc75cf2340b81e
Lucas Serralha <lucas.serralha@gmail.com>
2026-03-14 19:10:05 +0000

    Initial commit: Estrutura CRISP-DM criada pelo Aluno 1

---
+++ src/utils/profit_logic.py
@@ -0,0 +1,6 @@
+def calculate_profit(store, junior_count, expert_count, promo_pct, is_weekend):
+    """
+    Template para a função de avaliação (Slide 15).
+    A ser preenchido pelo Aluno 1 e validado com os dados de Baltimore.
+    """
+    pass
